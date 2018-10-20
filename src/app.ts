d33fd270c5b42034b72e715fd16fd8fa806fe4eb
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-10-20 18:03:51 +0700

    Initialize

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,9 @@
-import { Todo } from './todo';
+import { Note } from './note';
 
 export class App {
-  message = 'Hello World!';
-  todos: Todo[] = [];
-  todoDescription = '';
+  notes: Note[] = [];
+  selectedNoteId = 1;
+  searchNoteText = '';
 
-  addTodo() {
-    if (this.todoDescription) {
-      this.todos.push(new Todo(this.todoDescription));
-      this.todoDescription = '';
-    }
-  }
-
-  removeTodo(todo) {
-    let index = this.todos.indexOf(todo);
-
-    if (index > -1) {
-      this.todos.splice(index, 1);
-    }
-  }
+  createNote() {}
 }
