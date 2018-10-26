ef53b7ae9db30d8124f52fc77c663dc0a6233049
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-10-26 21:28:09 +0700

    Update value converters

--- src/resources/value-converters/title-format.ts
+++ src/resources/value-converters/title-format.ts
@@ -1,12 +1,11 @@
 export class TitleFormatValueConverter {
   toView(body) {
     const maxLength = 20;
+    const length = body.length;
 
-    if (!body) {
+    if (length === 0) {
       return 'New Note';
     }
-
-    const length = body.length;
 
     if (length > maxLength) {
       return body.substr(0, maxLength - 3) + '...';
